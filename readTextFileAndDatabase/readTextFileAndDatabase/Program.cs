9af587712da3ce0cd0ed0819001cfe1e5ac96fcd
lst2018GitHub <lsitorus2009@gmail.com>
2018-10-07 05:33:55 +0700

    Update readTextFileAndDatabase/readTextFileAndDatabase/Program.cs

--- readTextFileAndDatabase/readTextFileAndDatabase/Program.cs
+++ readTextFileAndDatabase/readTextFileAndDatabase/Program.cs
@@ -10,7 +10,7 @@ namespace readTextFileAndDatabase
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
+        [STAThread] 
         static void Main()
         {
             Application.EnableVisualStyles();
