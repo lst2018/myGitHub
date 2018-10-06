663d40fa00a03fd65f2624e1e1d4ee413b06ca29
lst2018GitHub <lsitorus2009@gmail.com>
2018-10-07 05:51:40 +0700

    Update readTextFileAndDatabase/readTextFileAndDatabase/Form1.designer.cs

--- readTextFileAndDatabase/readTextFileAndDatabase/Form1.designer.cs
+++ readTextFileAndDatabase/readTextFileAndDatabase/Form1.designer.cs
@@ -60,7 +60,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(141, 45);
+            this.label1.Location = new System.Drawing.Point(60, 53);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(641, 29);
             this.label1.TabIndex = 1;
