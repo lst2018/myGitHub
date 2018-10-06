49f8f65a1c7040bf9fa451219c4c52f2c171d7d1
lst2018GitHub <lsitorus2009@gmail.com>
2018-10-07 05:29:05 +0700

    Update readTextFileAndDatabase/readTextFileAndDatabase/Form1.designer.cs

---
+++ readTextFileAndDatabase/readTextFileAndDatabase/Form1.designer.cs
@@ -0,0 +1,110 @@
+﻿namespace readTextFileAndDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(268, 138);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Demo Read Data from Text File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(413, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(268, 138);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Read Data from database";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(141, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(475, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Simple Program for Cola Cola  Recruitment";
+            // 
+            // pb1
+            // 
+            this.pb1.BackColor = System.Drawing.Color.Transparent;
+            this.pb1.Image = global::readTextFileAndDatabase.Properties.Resources._8_2;
+            this.pb1.Location = new System.Drawing.Point(778, 2);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(23, 22);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pb1.TabIndex = 2;
+            this.pb1.TabStop = false;
+            this.pb1.Click += new System.EventHandler(this.pb1_Click);
+            this.pb1.MouseLeave += new System.EventHandler(this.pb1_MouseLeave);
+            this.pb1.MouseHover += new System.EventHandler(this.pb1_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Application Example";
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pb1;
+    }
+}
+
